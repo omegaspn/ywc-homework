aa8ea43e6dea762c17a8d60558ed8597ab3a9b9b
Supanat Wongchai <supanat.won@ascendcorp.com>
2019-11-01 15:50:46 +0700

    Updates

---
+++ precache-manifest.b173ab14d43a53d28e31887bd9f9f297.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db98408bf16f0540b92f9407dd7fcc0",
+    "url": "/ywc-homework/index.html"
+  },
+  {
+    "revision": "08e8b239fae6c48038d5",
+    "url": "/ywc-homework/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "474958fe02e80d85284e",
+    "url": "/ywc-homework/static/css/main.3bba31ac.chunk.css"
+  },
+  {
+    "revision": "08e8b239fae6c48038d5",
+    "url": "/ywc-homework/static/js/2.8b562044.chunk.js"
+  },
+  {
+    "revision": "474958fe02e80d85284e",
+    "url": "/ywc-homework/static/js/main.ab1e80dc.chunk.js"
+  },
+  {
+    "revision": "32fd00e6688e3a7f2236",
+    "url": "/ywc-homework/static/js/runtime-main.8d38ad5c.js"
+  },
+  {
+    "revision": "4727da78c4eaeb0b18f9ec621da88ea0",
+    "url": "/ywc-homework/static/media/TATSANA CHON-BLD.4727da78.TTF"
+  },
+  {
+    "revision": "ca27dc5d5160a4a4c0d30885f74cee40",
+    "url": "/ywc-homework/static/media/TATSANA CHON-REG.ca27dc5d.TTF"
+  },
+  {
+    "revision": "7c965f9a57a657b4b78228204b0e7960",
+    "url": "/ywc-homework/static/media/TATSANA SUKSA-REG.7c965f9a.TTF"
+  },
+  {
+    "revision": "376e338fc80b17f97347bd2d565af9cf",
+    "url": "/ywc-homework/static/media/TATSanaChon-Bold.376e338f.eot"
+  },
+  {
+    "revision": "45415b3d8382c9a11e16af5f88efab0e",
+    "url": "/ywc-homework/static/media/TATSanaChon-Bold.45415b3d.woff2"
+  },
+  {
+    "revision": "48b3589e049235e9698ef5ff23ef27ed",
+    "url": "/ywc-homework/static/media/TATSanaChon-Bold.48b3589e.woff"
+  },
+  {
+    "revision": "29c3fec8f8a81165e4a924a4f50df8d9",
+    "url": "/ywc-homework/static/media/TATSanaChon-Regular.29c3fec8.woff2"
+  },
+  {
+    "revision": "6000be56ca5133312622bfad9d8910d0",
+    "url": "/ywc-homework/static/media/TATSanaChon-Regular.6000be56.eot"
+  },
+  {
+    "revision": "62e175c727ab52c4f02269eda554a506",
+    "url": "/ywc-homework/static/media/TATSanaChon-Regular.62e175c7.woff"
+  },
+  {
+    "revision": "80a8dbbb4ef9ba317298ac50d3f07b58",
+    "url": "/ywc-homework/static/media/TATSanaSuksa-Regular.80a8dbbb.eot"
+  },
+  {
+    "revision": "93ecb34fae34be6a41d717baf2ee767d",
+    "url": "/ywc-homework/static/media/TATSanaSuksa-Regular.93ecb34f.woff"
+  },
+  {
+    "revision": "f51f1b81ee331401e0118c8e97c18083",
+    "url": "/ywc-homework/static/media/TATSanaSuksa-Regular.f51f1b81.woff2"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ywc-homework/precache-manifest.b173ab14d43a53d28e31887bd9f9f297.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ywc-homework/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
